93bdb4855cb384ad2292fbbf7fe899754b90dee6
EdgarPsda <edgar.psda@gmail.com>
2024-09-10 09:53:47 -0700

    Initial commit

---
+++ src/main/java/com/psda/creative_portfolio/CreativePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.psda.creative_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativePortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psda/creative_portfolio/CreativePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psda.creative_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
